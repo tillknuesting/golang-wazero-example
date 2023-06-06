72eac0fa5c3627cb940784227e6d8fa9ca9896d4
tillknuesting <tillknuesting@gmail.com>
2023-06-06 13:06:10 +0100

    na

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,17 +7,21 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	scanner.Scan() // use for reading line from Stdin
-	input := scanner.Text()
-
-	fmt.Println("From Stdin: ", input)
-
 	state := "0"
 	if len(os.Args) > 1 {
 		state = os.Args[1]
 	}
 
 	fmt.Println("From Args: ", state)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		fmt.Println("From Stdin: ", input)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
